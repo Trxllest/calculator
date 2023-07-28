da136e6a8e871319d9430ef9f7a3cf9e3cf0d517
David Olona <damilear@outlook.com>
2023-07-28 00:55:35 -0400

    Added basic math operations

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# A simple Calculator
+# A simple calculator
---
+++ calculator.js
@@ -0,0 +1,44 @@
+//////////////////////////////////////////
+// Global Variables
+let operator;
+let firstNum;
+let secondNum;
+
+
+
+
+//////////////////////////////////////////
+
+// add
+function add(a, b){return a + b;}
+
+// subtract
+function subtract(a, b){return a - b;}
+
+// multiply
+function multiply(a, b){return a * b;}
+
+// divide
+let divide =  (a, b) => b === 0 ? 'Cannot divide' : a / b;
+
+function operate(operator,num1,num2) {
+    let retval = 0;
+    if (operator === '/') {
+        retval = divide(num1,num2);
+    };
+    if (operator === '+') {
+        retval = add(num1,num2);
+    };
+    if (operator === '-') {
+        retval = subtract(num1,num2);
+    };
+    if (operator === '*') {
+        retval = multiply(num1,num2);
+    };
+    return Math.round(retval * 100) / 100
+}
+
+console.log(operate('/',2,3));
+console.log(operate('+',2,0));
+console.log(operate('-',2,0));
+console.log(operate('*',2,3));
