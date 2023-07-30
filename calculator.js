5883aa789bf3d369816abf83c0768be8a9c61d58
David Olona <damilear@outlook.com>
2023-07-30 14:26:13 -0400

    Add button events

--- calculator.js
+++ calculator.js
@@ -1,12 +1,14 @@
 //////////////////////////////////////////
 // Global Variables
-let operator;
-let firstNum;
-let secondNum;
-
-
-
-
+let operator = false;
+let firstNum = false;
+let secondNum = false;
+let operation = '' 
+let numBtns = document.querySelectorAll('.calcBtn.numBtn');
+let display = document.querySelector('.displayContent');
+let btns = document.querySelectorAll('.calcBtn');
+let operatorBtns = document.querySelectorAll('.calcBtn.operator');
+let allClearBtn = document.querySelector('.allClear');
 //////////////////////////////////////////
 
 // add
@@ -38,7 +40,71 @@ function operate(operator,num1,num2) {
     return Math.round(retval * 100) / 100 // limits to two decimal places if necessary
 }
 
-console.log(operate('/',2,3));
-console.log(operate('+',2,0));
-console.log(operate('-',2,0));
-console.log(operate('*',2,3));
+// console.log(operate('/',2,3));
+// console.log(operate('+',2,0));
+// console.log(operate('-',2,0));
+// console.log(operate('*',2,3));
+// let assignNum = function(e) {
+//     if (!firstNum) {
+//         firstNum = e.textContent;
+//         operation = operation + firstNum;
+//         console.log(firstNum);
+//         console.log(operation);
+//     }
+
+//     if (firstNum && operator) {
+//         secondNum = e.textContent;
+//         operation = operation + secondNum;
+//         console.log();
+//     }
+// }
+
+// let assignOperator = function(e) {
+//     if (firstNum) {
+//         operator = e.textContent;
+//     }
+//     operation = operation + ` ${operator} `;    
+// }
+
+for (let btn of numBtns) {
+    btn.addEventListener('click', (e) => {
+        if (!firstNum) {
+            firstNum = e.textContent;
+            operation = operation + firstNum;
+            console.log(firstNum);
+            console.log(operation);
+        }
+    
+        if (firstNum && operator) {
+            secondNum = e.textContent;
+            operation = operation + secondNum;
+            console.log();
+        }
+    });
+}
+
+for (let op of operatorBtns) {
+    op.addEventListener('click', (e) => {
+        if (firstNum) {
+            operator = e.textContent;
+        }
+        operation = operation + ` ${operator} `;
+        console.log(operation);
+    });
+}
+
+// Clear all
+
+let allClear = function() {
+    operation = '';
+    display.textContent = operation;
+    firstNum = false;
+    secondNum = false;
+    operator = false;
+    // console.log(firstNum);
+};
+
+allClearBtn.addEventListener('click', allClear);
+
+// console.log(operation);
+
